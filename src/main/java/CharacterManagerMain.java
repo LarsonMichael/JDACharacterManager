aa05a5d9c450fd8636138d9534fef03b01530f3a
LarsonMichael <46038702+LarsonMichael@users.noreply.github.com>
2018-12-20 21:39:02 -0600

    Initial commit

---
+++ src/main/java/CharacterManagerMain.java
@@ -0,0 +1,31 @@
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+import javax.security.auth.login.LoginException;
+
+public class CharacterManagerMain extends ListenerAdapter {
+    public static void main(String[] args) throws LoginException {
+        JDABuilder builder = new JDABuilder(AccountType.BOT);
+        builder.setToken("NTI1MTI4MzI3NzYyMTQ5Mzkx.DvyHqw.o4WtKRX5KOgQOdHfypzhiaNQd18");
+        builder.addEventListener(new CharacterManagerMain());
+        builder.build();
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if (event.getAuthor().isBot() || event.getAuthor().isFake()) {
+            return;
+        }
+
+        System.out.println("We received a message from " +
+                event.getAuthor().getName() + ": " +
+                event.getMessage().getContentDisplay()
+        );
+
+        if (event.getMessage().getContentRaw().equalsIgnoreCase("!ping")) {
+            event.getChannel().sendMessage("Pong!").queue();
+        }
+    }
+}
