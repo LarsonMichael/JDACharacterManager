8ca6ecc09ddd9dab7a7f024242661adc366f4486
LarsonMichael <46038702+LarsonMichael@users.noreply.github.com>
2019-01-21 10:55:47 -0600

    Extra line in HELP command output, for clarity

--- src/main/java/commands/HelpCommandHandler.java
+++ src/main/java/commands/HelpCommandHandler.java
@@ -6,7 +6,7 @@ public class HelpCommandHandler implements CommandHandler {
     private final String HELP_TEXT = "This bot is designed to help manage character stats and rolls for 5th edition.\n" +
             "Characters are tracked per user per server. Multiple characters can be owned by one user on a server,\n" +
             "but only one will be considered the current context at any given time.\n" +
-            "For help with a particular command, use ![COMMAND] -h or ![COMMAND] --help for information on that command\n" +
+            "For help with a particular command, use ![COMMAND] -h or ![COMMAND] --help for information on that command\n\n" +
             "The Character Manager bot accepts the following commands:\n" +
             "!help         : Prints the help text you are seeing. !help does not have in depth documentation for -h\n" +
             "!character    : Switches the current context to a provided character name. If that character doesn't exist it is created with default stats";
